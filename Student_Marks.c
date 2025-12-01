608b84a46ca7eb1bb08c28718ef37aeb386fa66f
Keyur Pipaliya123 <112421106+Keyurpipaliya@users.noreply.github.com>
2025-12-01 16:29:45 +0530

    Add files via upload

---
+++ Student_Marks.c
@@ -0,0 +1,46 @@
+// Create an HeaderFile
+#include<stdio.h>
+int main() {
+
+    // create is declare an datatypes in Variables
+    int Students[5];
+    char Name;
+    int Members;
+    int Roll_No;
+    int Chemestry_Marks;
+    int Physics_Marks;
+    int Maths_Marks;
+    float percentage;
+
+    printf("Please Your Enter is 5 Students Marks Choice %d\n : ");
+    {
+ 
+    // create is for loop
+    for(int i = 0; i <= 5; i++) {
+
+    // To Run is While Loops
+        while (percentage == 5)
+        {
+            printf("Enter is First students 1 Subjects of Marks \n : ");
+            scanf("%d", &Students);
+
+            printf("Enter is Second students 2 Subjets of Marks \n : ");
+            scanf("%d", &Students);
+
+            printf("Enter is third students 3 Subjects of Marks \n : ");
+            scanf("%d", &Students);
+
+            printf("Enter is fourth students 4 Subjects of Marks \n : ");
+            scanf('%d', &Students);
+
+            printf("Enter is Five students 5 Subjects of Marks  \n : ");
+            scanf("%d", &Students);
+        }
+        printf("Enter is 5 students subjects of highest percentage :");
+
+    }
+
+    return 0;
+}
+
+}
